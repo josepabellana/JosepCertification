498dce27300db8b4f03e110fa2fcb97f1c870bd6
josepabellana <josep.abe.pu@gmail.com>
2023-06-10 02:01:26 +0200

    x

--- dst/index.js
+++ dst/index.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const playwright_1 = require("playwright");
-const utils_1 = require("./utils");
 const launchOptions = {
     slowMo: 0,
     args: ['--use-fake-ui-for-media-stream', '--use-fake-device-for-media-stream', '--deny-permission-prompts'],
@@ -57,25 +56,23 @@ async function getManifestURL(url) {
     });
 }
 // Llamar a la función y proporcionar la URL de la página web
-const url = 'https://hivejsartifacts.blob.core.windows.net/artifacts/plugins/102147/html5/dist/reference/html5/9.2.0/hivejs/silent-videojs.test.html?manifest=https://streaming-simulator-prod.hivestreaming.com/generic/live/beta-big-bunny-multi/manifest.m3u8?callback=https://api.hivestreaming.com/v1/events/9001/15/1894266/mWnCvsg73dQRIfoj';
-getManifestURL(url)
-    .then(({ manifestContent, playlistContent }) => {
-    // console.log(manifestContent);
-    if (manifestContent) {
-        let manifestBitrates = (0, utils_1.extractResolutionsFromManifest)(manifestContent);
-        if (manifestBitrates && manifestBitrates.length > 0) {
-            console.log('This manifest has:', manifestBitrates.length);
-            console.log('The highest Bitrate is:', manifestBitrates[0]);
-        }
-    }
-    if (playlistContent) {
-        let { targetDuration, fragmentCount } = (0, utils_1.parsePlaylistContent)(playlistContent);
-        if (targetDuration)
-            console.log('The playlist have the following fragment duration:', targetDuration);
-        if (fragmentCount)
-            console.log('The amount of fragments are: ', fragmentCount);
-    }
-})
-    .catch((error) => {
-    console.error('Error:', error);
-});
+// const url = 'https://hivejsartifacts.blob.core.windows.net/artifacts/plugins/102147/html5/dist/reference/html5/9.2.0/hivejs/silent-videojs.test.html?manifest=https://streaming-simulator-prod.hivestreaming.com/generic/live/beta-big-bunny-multi/manifest.m3u8?callback=https://api.hivestreaming.com/v1/events/9001/15/1894266/mWnCvsg73dQRIfoj';
+// getManifestURL(url)
+//   .then(({manifestContent, playlistContent}) => {
+//     // console.log(manifestContent);
+//     if (manifestContent) {
+//      let manifestBitrates = extractResolutionsFromManifest(manifestContent);
+//      if(manifestBitrates && manifestBitrates.length > 0){
+//         console.log('This manifest has:', manifestBitrates.length);
+//         console.log('The highest Bitrate is:', manifestBitrates[0]);
+//      }
+//     }
+//     if(playlistContent){
+//       let {targetDuration, fragmentCount} = parsePlaylistContent(playlistContent);
+//       if(targetDuration) console.log('The playlist have the following fragment duration:', targetDuration);
+//       if(fragmentCount) console.log('The amount of fragments are: ', fragmentCount);
+//     }
+//   })
+//   .catch((error) => {
+//     console.error('Error:', error);
+//   });
