2fe0990d08071e17ddefeb8c5ab7ef95f8534fc1
josepabellana <josep.abe.pu@gmail.com>
2023-06-13 20:03:57 +0200

    x

--- src/server/support/utils.ts
+++ src/server/support/utils.ts
@@ -34,3 +34,21 @@ export function parsePlaylistContent(playlistContent: string): { targetDuration:
   
     return { targetDuration, fragmentCount };
   }
+
+
+
+const config = {
+
+  "HLS": {
+    "ticket": "https://api-test.hivestreaming.com/v1/events/9001/15/2060/cV1AMV4P9FOyf3cl",
+    "manifest": "https://streaming-simulator-prod.hivestreaming.com/generic/live/beta-big-bunny-multi/manifest.m3u8"
+  },
+  "DASH": {
+    "ticket": "https://api-test.hivestreaming.com/v1/events/9001/6/1842/8KAiaYDvMgfJAD24",
+    "manifest": "https://streaming-simulator-prod.hivestreaming.com/wams/live/beta-llamaDrama.ism/manifest(format=mpd-time-csf)"
+  },
+  "WOWZA": {
+    "ticket": "https://api-test.hivestreaming.com/v1/events/9001/15/1561/TqqkQLKAJDEN5hLX",
+    "manifest": "https://wowzaec2demo.streamlock.net/live/bigbuckbunny/manifest_mpm4sav_mvtime.mpd"
+  }
+}
