b5fdba73847af208628979e24a4c37c1819577c9
josepabellana <josep.abe.pu@gmail.com>
2023-06-15 00:00:23 +0200

    x

--- Client/index.js
+++ Client/index.js
@@ -1,15 +1,21 @@
 
 function checkManifestType(link){
-    fetch(manifestUrl)
+    fetch(link)
     .then(response => {
       const contentType = response.headers.get("content-type");
+      let manifestType;
       if (contentType && contentType.includes("application/vnd.apple.mpegurl")) {
         console.log("This is an HLS manifest.");
+        manifestType = "HLS";
       } else if (contentType && contentType.includes("application/dash+xml")) {
         console.log("This is a Dash manifest.");
+        manifestType = "Dash";
       } else {
         console.log("Unknown manifest type.");
       }
+      const manifestDiv = document.createElement("div");
+      manifestDiv.textContent = `Manifest Type: ${manifestType}`;
+      document.getElementsByClassName('button_info_container')body.appendChild(manifestDiv);
     })
     .catch(error => {
       console.error("Error fetching manifest:", error);
